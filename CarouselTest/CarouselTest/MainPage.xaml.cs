bde3a327cdd7591758eb3a876c85993a1439bf2a
Kevin Han <main.share.us@gmail.com>
2018-11-26 20:57:42 -0600

    Add project files.

---
+++ CarouselTest/CarouselTest/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CarouselTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModels.MainPageViewModel();
+            List<string> mylist = new List<string>(new string[] { "page1", "page2", "page3", "page4" });
+            view.ItemsSource = mylist;
+    }
+    }
+}
---
+++ CarouselTest/CarouselTest/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarouselTest.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public string Image { get; set; } = "icon.png";
+    }
+}
